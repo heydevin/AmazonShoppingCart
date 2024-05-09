6c003c5ffa0d599be170f917295c6d7a0a5f3502
Devinlin9 <devinlin@students.cs.ubc.ca>
2024-05-08 18:16:12 -0700

    Fixing invalid date error

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -37,8 +37,9 @@ cart.forEach((cartItem) =>{
   });
   
   const today = dayjs();
-  const deliveryDate = today.add(deliveryOptions.deliveryDays, 'days');
+  const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
   const dateString = deliveryDate.format('dddd, MMMM D');
+  console.log(deliveryDate);
 
   cartSummaryHTML += `
     <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
