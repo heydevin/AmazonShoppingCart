e718f071faabc97aacb612edfb377e34e77f6f7b
Devinlin9 <devinlin@students.cs.ubc.ca>
2024-05-16 00:37:47 -0700

    Adjustments made on orderSummaryTest for better formatting, readability, and maintainability

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -4,11 +4,14 @@ import {loadFromStorage, cart} from '../../data/cart.js'
 describe('test suites: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
-  
+  //Hooks in Jasmine
   beforeEach(() => {
+    spyOn(localStorage, 'setItem');
+
     document.querySelector('.js-test-container').innerHTML = `
-        <div class="js-order-summary"></div>
-      `;
+      <div class="js-order-summary"></div>
+      <div class="js-payment-summary"></div>
+    `;
 
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([{
@@ -24,6 +27,10 @@ describe('test suites: renderOrderSummary', () => {
     loadFromStorage();
 
     renderOrderSummary();
+  });
+
+  afterEach(() => {
+    document.querySelector('.js-test-container').innerHTML = ``;
   });
 
   it('displays the cart', () => {
@@ -37,35 +44,9 @@ describe('test suites: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-product-quantity-${productId2}`).innerText
     ).toContain('Quantity: 1');
-
-    document.querySelector('.js-test-container').innerHTML = ``;
   });
 
   it('removes a product', () => {
-    spyOn(localStorage, 'setItem');
-
-    document.querySelector('.js-test-container').innerHTML = `
-      <div class="js-order-summary"></div>
-      <div class="js-payment-summary"></div>
-    `;
-
-    const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-    const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
-    spyOn(localStorage, 'getItem').and.callFake(() => {
-      return JSON.stringify([{
-        productId: productId1,
-        quantity: 2,
-        deliveryOptionId: '1'
-      }, {
-        productId: productId2,
-        quantity: 1,
-        deliveryOptionId: '2'
-      }]);
-    });
-    loadFromStorage();
-
-    renderOrderSummary();
-
     document.querySelector(`.js-delete-link-${productId1}`).click();
 
     expect(
@@ -79,7 +60,5 @@ describe('test suites: renderOrderSummary', () => {
     ).not.toEqual(null);
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
-
-    document.querySelector('.js-test-container').innerHTML = ``;
   });
 });
