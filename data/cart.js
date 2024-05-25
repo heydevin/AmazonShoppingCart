0acc2e9f9585de52435079fd3a5089c8dfacb28d
Devinlin9 <devinlin@students.cs.ubc.ca>
2024-05-25 07:42:06 -0700

    Learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -71,3 +71,15 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
   
   saveToStorage();
 }
+
+export function loadCart(renderFunction) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response)
+    renderFunction();
+  });
+
+  xhr.open('GET','https://supersimplebackend.dev/cart');
+  xhr.send();
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,53 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/backend-practice.js';
 
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('Value 1');
+    });
+  
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((value) => {
+  console.log(value);
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('Value 1');
+  });
+
+}).then((value) => {
+  console.log(value);
+
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+// Multiple callbacks cause a lot of nesting
+/*
 loadProducts(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
